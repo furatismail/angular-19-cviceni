76f3fa25ef0b5f2b0a6cc01001e1049b5ff320c6
Furát Ismail <furat.ismail@cadcom.cz>
2025-05-18 23:04:53 +0200

    resource

--- src/app/features/signals/desserts/desserts.component.ts
+++ src/app/features/signals/desserts/desserts.component.ts
@@ -1,13 +1,13 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, computed, inject, OnDestroy, OnInit, signal, effect, untracked } from '@angular/core';
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { Component, computed, inject, OnDestroy, OnInit, signal, effect, untracked, resource } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { catchError, of, startWith, Subscription, switchMap, tap } from 'rxjs';
+import { catchError, firstValueFrom, of, startWith, Subscription, switchMap, tap } from 'rxjs';
 import { DessertService } from '../../../core/services/dessert.service';
 import { Dessert, DessertFilter } from '../../../shared/interfaces/models/dessert.model';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { rxResource, toObservable, toSignal } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-desserts',
-  imports: [FormsModule, NgIf, NgFor],
+  imports: [FormsModule, NgIf, NgFor, JsonPipe],
   templateUrl: './desserts.component.html',
   styleUrl: './desserts.component.css'
 })
@@ -17,42 +17,21 @@ export class DessertsComponent implements OnInit, OnDestroy {
   englishName = signal<string>('');
   loading = signal<boolean>(false);
 
-  filter = computed(() => {
+  filter = computed<DessertFilter>(() => {
     return {
       originalName: this.originalName(),
       englishName: this.englishName(),
     }
   })
 
-  // Vytvoření signálu desserts, který reaguje na změny filtru a načítá data z API
-  desserts = toSignal(
-    // Konverze computed signálu `filter` na RxJS observable
-    toObservable(this.filter).pipe(
-      // Před každým voláním API nastavíme stav načítání na true
-      tap(() => this.loading.set(true)),
-      // Při změně filtru spustíme volání služby
-      switchMap((filter: DessertFilter) => {
-        return this.dessertService.find(filter).pipe(
-          // Pokud volání proběhne úspěšně, nastavíme loading na false
-          tap(() => this.loading.set(false)),
+  // Toto je špatně pro rxResource – použij jen pro resource()
+  dessertsRx = rxResource<Dessert[], DessertFilter>({
+    request: () => this.filter(),
+    loader: ({ request: filter }) => this.dessertService.find(filter),
+  });
 
-          // Pokud dojde k chybě, vypíšeme ji a loading nastavíme na false
-          catchError(err => {
-            console.error('Catch error', err);
-            this.loading.set(false);
-            return of([]); // Vracíme prázdné pole, aby komponenta nezkolabovala
-          })
-        )
-      }
-      ),
 
-      // Na začátku (např. při mountu komponenty) nastavíme počáteční hodnotu na []
-      startWith([])
-    ),
 
-    // Požadovaná počáteční hodnota pro toSignal
-    { initialValue: [] }
-  );
 
 
 
@@ -60,11 +39,14 @@ export class DessertsComponent implements OnInit, OnDestroy {
   private readonly subscription = new Subscription();
 
   constructor() {
-
+    effect(() => {
+        console.log(this.dessertsRx)
+    })
   }
 
   ngOnInit(): void {
   }
+
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
