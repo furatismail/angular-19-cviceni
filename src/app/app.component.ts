6aedcce3215a8f4ba0f8e9d5025365614b46ee70
Furát Ismail <furat.ismail@cadcom.cz>
2025-05-18 17:04:05 +0200

    signal-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TimeComponent } from './shared/components/time/time.component';
-import { SignalSearchComponent } from './features/signals/signal-search.component';
+import { SignalSearchComponent } from './features/signals/signal-search/signal-search.component';
+import { IsEvenComponent } from './features/signals/is-even/is-even.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, TimeComponent, SignalSearchComponent],
+  imports: [RouterOutlet, RouterLink, TimeComponent, SignalSearchComponent, IsEvenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
